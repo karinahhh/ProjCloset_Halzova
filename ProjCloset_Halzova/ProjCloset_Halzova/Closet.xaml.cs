43e277030572abec887fa6b2de0c838392ee8c46
Opilane TTHK <opilane@tthk.ee>
2021-02-19 15:40:21 +0200

    Add project files.

---
+++ ProjCloset_Halzova/ProjCloset_Halzova/Closet.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjCloset_Halzova
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Closet : ContentPage
+    {
+        public Closet()
+        {
+            InitializeComponent();
+        }
+    }
+}
